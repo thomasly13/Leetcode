37cfda6ed9fc1470a768f0ef8b1a7eb936d0cd45
Thomas Ly <Thomaslyy13@gmail.com>
2023-08-06 16:10:47 -0700

    added baseball game

--- leetCode.rb
+++ leetCode.rb
@@ -85,3 +85,24 @@ def merge(intervals)
 
 # };
 
+# Baseball Game
+def cal_points(operations)
+
+    stack = []
+
+    operations.each do |op|
+        if op == "+"
+            stack.push(stack[-1] + stack[-2])
+        elsif op == "D"
+            stack.push(stack[-1] * 2)
+        elsif op == "C"
+            stack.pop()
+        else 
+            stack.push(op.to_i)
+        end
+    end
+
+    return stack.sum
+    
+end
+
