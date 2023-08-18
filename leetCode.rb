2a7a22ff16ddd2cdf18b128945aebe751aef831f
Thomas Ly <Thomaslyy13@gmail.com>
2023-08-18 16:57:32 -0700

    added letter combinations

--- leetCode.rb
+++ leetCode.rb
@@ -773,3 +773,33 @@ def search(nums, target)
     end
     
 end
+
+# letter combinations
+
+#var letterCombinations = function(digits) {
+    #let keypad = {
+        #2: ["a", "b", "c"],
+        #3: ["d", "e", "f"],
+        #4: ["g", "h", "i"],
+        #5: ["j", "k", "l"],
+        #6: ["m", "n", "o"],
+        #7: ["p", "q", "r", "s"],
+        #8: ["t", "u", "v"],
+        #9: ["w", "x", "y", "z"]
+    #}
+    #let results = [];
+    #if (digits.length === 0) return [];
+    #let array = digits.split("");
+    #let letters = keypad[array.shift()];
+    #let newArray = letterCombinations(array.join(""));
+    #for (let i = 0; i < letters.length; i++) {
+        #if (newArray.length === 0) {
+            #results.push(letters[i])
+        #} else {
+            #for (let j = 0; j < newArray.length; j++) {
+                #results.push(letters[i] + newArray[j])
+            #}
+        #}
+    #}
+    #return results;
+#};
