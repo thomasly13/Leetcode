6eb225b4843398a55a0ee95ac5c78e855dfcb34c
Thomas Ly <Thomaslyy13@gmail.com>
2024-02-07 23:29:57 -0800

    added perfect squares

--- leetCode.rb
+++ leetCode.rb
@@ -2646,3 +2646,11 @@ def frequency_sort(s)
     h.keys.sort.reverse.each {|key| result += h[key] }
     result
 end
+
+# perfect squares
+def num_squares(n)
+    n /= 4 while n % 4 == 0
+    return 4 if n % 8 == 7
+    return 3 if n.prime_division.any? { |p, e| p % 4 == 3 && e.odd? }
+    (n**0.5).to_i**2 == n ? 1 : 2
+  end
