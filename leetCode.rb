b758c4dcbb93ffbf595787db5fc6b8d7e9f2df6c
Thomas Ly <Thomaslyy13@gmail.com>
2024-02-16 18:41:55 -0800

    added find polygon with the largest perimeter

--- leetCode.rb
+++ leetCode.rb
@@ -2729,3 +2729,46 @@ def largestPerimeter(nums)
     end
     return ans
 end
+
+
+def furthest_building(heights, bricks, ladders)
+    left = 0
+    right = heights.count - 1
+    result = 0
+    
+    while left <= right
+      mid = (left + right)/2
+      if can_reach?(heights[0...mid+1], bricks, ladders)
+        result = mid
+        left = mid + 1
+      else
+        right = mid - 1
+      end
+    end
+    
+    result
+  end
+  
+  def can_reach?(heights, bricks, ladders)
+    diffs = []
+    n = heights.count
+    
+    heights.each_with_index do |h, i|
+      break if i >= n - 1
+      diffs << heights[i+1] - h if h < heights[i+1]
+    end
+    
+    diffs.sort!
+    
+    diffs.each do |diff|
+      if bricks >= diff
+        bricks -= diff
+      elsif ladders > 0
+        ladders -= 1
+      else
+        return false
+      end
+    end
+    
+    true
+  end
