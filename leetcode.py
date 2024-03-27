2e0a23bbd06664691abce125f5aa86ee0da1b520
Thomas Ly <Thomaslyy13@gmail.com>
2024-03-26 21:04:45 -0700

    added Merge alternatively

--- leetcode.py
+++ leetcode.py
@@ -20,3 +20,27 @@ def loop(array):
 array = ["Aligator", "Bannana", "Cat", "Dolphin"]
 
 loop(array)
+
+
+# Merge alternatively
+    def mergeAlternately(self, word1, word2):
+        final_word = ""
+
+        word1_len = len(word1)
+        word2_len = len(word2)
+
+        counter1 = 0
+
+        counter2 = 0
+
+        while counter1 < word1_len and counter2 < word2_len:
+            final_word += word1[counter1] + word2[counter2]
+            counter1 += 1
+            counter2 += 1
+
+        if counter1 == word1_len:
+            final_word += word2[counter2:]
+        else: 
+            final_word += word1[counter1:]
+        
+        return final_word
