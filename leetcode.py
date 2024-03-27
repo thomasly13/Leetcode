649133458c3f5483a0dd515985aa82fbdcb4eadf
Thomas Ly <Thomaslyy13@gmail.com>
2024-03-26 20:52:15 -0700

    added more python practice

--- leetcode.py
+++ leetcode.py
@@ -4,3 +4,19 @@ def test(arg1, arg2):
     return arg1 + arg2 
 
 print(test(1, 2))
+
+
+def concat(str1, str2):
+    return (f'I am a {str1}, but also a {str2}')
+
+print(concat("dolphin", "gorilla"))
+
+
+def loop(array):
+    for w in array:
+        print(w)
+
+
+array = ["Aligator", "Bannana", "Cat", "Dolphin"]
+
+loop(array)
