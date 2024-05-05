ca9678d4420c96d25641b329faae59f07b8eca33
Thomas Ly <Thomaslyy13@gmail.com>
2024-05-04 18:58:03 -0700

    added combination sum

--- leetCode.rb
+++ leetCode.rb
@@ -3799,3 +3799,17 @@ def delete_node(node)
     node.val = node.next.val
     node.next = node.next.next
 end
+
+# Combination Sum 
+def combination_sum(candidates, target)
+    candidates.sort.flat_map do |candidate|
+     if candidate == target
+         [[candidate]]
+     elsif candidate < target
+         combination_sum(candidates.select { |c| c >= candidate }, target - candidate)
+             .map { |combo| [candidate] + combo }
+     else
+         []
+     end
+ end
+end
