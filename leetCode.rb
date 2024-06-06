c6fa52ca1734cfcd9016cdaaab52d04dc3aaef61
Thomas Ly <Thomaslyy13@gmail.com>
2024-06-05 23:11:45 -0700

    added is straight hand

--- leetCode.rb
+++ leetCode.rb
@@ -4203,3 +4203,29 @@ def common_chars(words)
     
     result
 end
+
+# is straight hand
+def is_n_straight_hand(hand, group_size)
+    if (hand.size % group_size != 0)
+        return false
+    end
+
+    hand.sort!
+    while hand.size > 0
+        count = 1
+        last_num = hand.shift
+        while count < group_size
+            next_num = last_num + 1
+            index = hand.bsearch_index{|n| next_num <=> n}
+            if !index
+                return false
+            else
+                hand.delete_at(index)
+            end
+            last_num = next_num
+            count = count + 1
+        end
+    end
+
+    return true
+end
