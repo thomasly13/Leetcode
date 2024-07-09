9a72200154cf02d076c2590e2ccc2ee53312f3c7
Thomas Ly <Thomaslyy13@gmail.com>
2024-07-08 22:25:50 -0700

    added python average wait time

--- leetcode.py
+++ leetcode.py
@@ -103,3 +103,20 @@ loop(array)
         return False
 
 
+# Average Wait Time
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        currentTime = 0
+        totalwaitTime = 0
+        
+        for customer in customers:
+            arrival, time = customer
+            
+            if currentTime < arrival:
+                currentTime = arrival
+                
+            waitTime = currentTime + time - arrival
+            totalwaitTime += waitTime
+            
+            currentTime += time
+        
+        return totalwaitTime / len(customers)
