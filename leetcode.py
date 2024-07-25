76fba83d9799b2e9c68a04380853a8a0acd67230
Thomas Ly <Thomaslyy13@gmail.com>
2024-07-24 21:28:47 -0700

    adde python sort array by quick sort

--- leetcode.py
+++ leetcode.py
@@ -255,3 +255,21 @@ def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeN
     def frequencySort(self, nums):
         freq = Counter(nums)
         return sorted(nums, key=lambda x : (freq[x], -x))
+
+
+# Sort array using quick sort
+    def sortArray(self, N: List[int]) -> List[int]:
+        def quicksort(A, I, J):
+            if J - I <= 1: return
+            p = partition(A, I, J)
+            quicksort(A, I, p), quicksort(A, p + 1, J)
+        
+        def partition(A, I, J):
+            A[J-1], A[(I + J - 1)//2], i = A[(I + J - 1)//2], A[J-1], I
+            for j in range(I,J):
+                if A[j] < A[J-1]: A[i], A[j], i = A[j], A[i], i + 1
+            A[J-1], A[i] = A[i], A[J-1]
+            return i
+        
+        quicksort(N,0,len(N))
+        return N
