cf31ef1211a98b7d142ab24110c3777d2d595075
Thomas Ly <Thomaslyy13@gmail.com>
2024-07-25 16:30:35 -0700

    added Python Count and Say

--- leetcode.py
+++ leetcode.py
@@ -273,3 +273,20 @@ def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeN
         
         quicksort(N,0,len(N))
         return N
+
+
+# Count and Say
+def countAndSay(self, n):
+        s = '1'
+        for _ in range(n-1):
+            let, temp, count = s[0], '', 0
+            for l in s:
+                if let == l:
+                    count += 1
+                else:
+                    temp += str(count)+let
+                    let = l
+                    count = 1
+            temp += str(count)+let
+            s = temp
+        return s
