3b8f729596eed7d3304a0d28142b81d8c75e1384
Thomas Ly <Thomaslyy13@gmail.com>
2024-08-03 17:17:17 -0700

    added Python Range Sum of Sorted Subarray Sums

--- leetcode.py
+++ leetcode.py
@@ -320,3 +320,15 @@ def countAndSay(self, n):
             cnt -= nums[(i - k + n) % n]
             mx = max(mx, cnt)
         return k - mx
+
+
+# Range Sum of Sorted Subarray Sums
+def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+    l = []
+    for i in range(len(nums)):
+        cum = 0
+        for j in range(i,len(nums)):
+            cum+=nums[j]
+            l.append(cum)
+    l.sort()
+    return sum(l[left-1:right])%(10**9+7)
