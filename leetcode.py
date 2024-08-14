59a1fcb18d5711b86447cb438cc8c95af30d01c8
Thomas Ly <Thomaslyy13@gmail.com>
2024-08-13 23:56:18 -0700

    added python smaller pair sdistance

--- leetcode.py
+++ leetcode.py
@@ -446,3 +446,27 @@ def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
                 if subgrid in magic_sq:
                     cnt += 1
         return cnt
+
+
+# Smallest pair distance
+   def smallestDistancePair(self, nums, k):
+        nums.sort()
+        n = len(nums)
+        low, high = 0, nums[-1] - nums[0]
+
+        def count_pairs(max_distance):
+            count, j = 0, 0
+            for i in range(n):
+                while j < n and nums[j] - nums[i] <= max_distance:
+                    j += 1
+                count += j - i - 1
+            return count
+
+        while low < high:
+            mid = (low + high) // 2
+            if count_pairs(mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
