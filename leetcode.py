19e0e18c0b76cfef992af53eecb7a3743a14998f
Thomas Ly <Thomaslyy13@gmail.com>
2024-08-19 20:53:43 -0700

    added stone game 2

--- leetcode.py
+++ leetcode.py
@@ -481,4 +481,26 @@ def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
                 steps += i
                 n //= i
             i += 1
-        return steps
+        return 
+        
+
+# stone game 2
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        
+        dp = [[0] * (n + 1) for _ in range(n)]
+        suffix_sum = [0] * n
+        suffix_sum[-1] = piles[-1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + piles[i]
+        
+        for i in range(n - 1, -1, -1):
+            for m in range(1, n + 1):
+                if i + 2 * m >= n:
+                    dp[i][m] = suffix_sum[i]
+                else:
+                    for x in range(1, 2 * m + 1):
+                        dp[i][m] = max(dp[i][m], suffix_sum[i] - dp[i + x][max(m, x)])
+        
+        return dp[0][1]
