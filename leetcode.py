ac2838276716737b0c3f3aca175a9b8f45c67b8c
Thomas Ly <Thomaslyy13@gmail.com>
2024-09-02 20:00:49 -0700

    added python sum of digits of string after conversion

--- leetcode.py
+++ leetcode.py
@@ -586,3 +586,19 @@ class Solution:
         uniqueComponents = {uf.find(i) for i in range(n)}
 
         return n - len(uniqueComponents)
+
+    # Sum of digits of string after conversion
+        def getLucky(self, s, k):
+        # Convert each character in the string to its corresponding numeric value
+        number = ''
+        for x in s:
+            number += str(ord(x) - ord('a') + 1)
+        
+        # Perform the transformation `k` times
+        while k > 0:
+            temp = 0
+            for x in number:
+                temp += int(x)  # Sum the digits of the current number
+            number = str(temp)  # Convert the sum back to a string
+            k -= 1
+        return int(number)  # Return the final result as an integer
