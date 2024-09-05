5915a9faff2729837e654207aaebd67e31deef2f
Thomas Ly <Thomaslyy13@gmail.com>
2024-09-04 22:27:27 -0700

    added python find mission observations

--- leetcode.py
+++ leetcode.py
@@ -602,3 +602,23 @@ class Solution:
             number = str(temp)  # Convert the sum back to a string
             k -= 1
         return int(number)  # Return the final result as an integer
+
+        # Find Missing Obervations
+            def missingRolls(self, rolls, mean, n):
+        m = len(rolls)
+        total_sum = mean * (n + m)
+        observed_sum = sum(rolls)
+        
+        missing_sum = total_sum - observed_sum
+        
+        if missing_sum < n or missing_sum > 6 * n:
+            return []
+        
+        base = missing_sum // n
+        remainder = missing_sum % n
+        
+        result = [base] * n
+        for i in range(remainder):
+            result[i] += 1
+        
+        return result
