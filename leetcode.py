c81b1fdbb6a691ff6da7c44ed74718cf8049c284
Thomas Ly <Thomaslyy13@gmail.com>
2024-09-14 15:59:16 -0700

    added python longest sub array with maximum bitsize AND

--- leetcode.py
+++ leetcode.py
@@ -667,4 +667,21 @@ class Solution:
         
         return result
 
+    # Longest Sub Array with amximum Bitsize AND   
+        def longestSubarray(self, nums: List[int]) -> int:
+        maxBitwiseAnd = max(nums)
+        maxi = 1
+        count = 0
+        i = 0
         
+        while i < len(nums):
+            if nums[i] == maxBitwiseAnd:
+                while i < len(nums) and nums[i] == maxBitwiseAnd:
+                    count += 1
+                    i += 1
+                maxi = max(maxi, count)
+                count = 0
+            else:
+                i += 1
+        
+        return maxi
