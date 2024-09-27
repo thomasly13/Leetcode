90bb101731938de55a28eb5a719eae9e25b7d64a
Thomas Ly <Thomaslyy13@gmail.com>
2024-09-26 22:53:57 -0700

    added python calendar 2

--- leetcode.py
+++ leetcode.py
@@ -768,3 +768,65 @@ class Solution:
                 total_score += node['$']
             scores.append(total_score)
         return scores
+
+
+    # My calendar 2
+    class MyCalendarTwo:
+
+    def __init__(self):
+        self.single_booked = []
+        self.double_booked = []
+        
+    def intersection(self, intervals, s, e):
+
+        l = bisect.bisect_left(intervals, s)
+        r = bisect.bisect_right(intervals, e)
+        
+        intersection = []
+        
+        if l % 2:
+            if intervals[l] != s:
+                intersection.append(s)
+            else:
+                l = l + 1
+
+        intersection += intervals[l:r]
+
+        if r % 2:
+            if intervals[r-1] != e:
+                intersection.append(e)
+            else:
+                intersection.pop()
+
+        return intersection
+        
+    def add(self, intervals, s, e):
+
+        l = bisect.bisect_left(intervals, s)
+        r = bisect.bisect_right(intervals, e)
+        
+        new = []
+        if not l % 2:
+            new.append(s)
+            
+        if not r % 2:
+            new.append(e)
+
+        intervals[l:r] = new
+
+    def book(self, start: int, end: int) -> bool:
+
+        if self.intersection(self.double_booked, start, end):
+            return False
+        
+        intersection = self.intersection(self.single_booked, start, end)
+
+        if intersection:
+            for i in range(len(intersection) // 2):
+                i1 = intersection[2*i]
+                i2 = intersection[2*i+1]
+                self.add(self.double_booked, i1, i2)
+
+        self.add(self.single_booked, start, end)
+
+        return True
