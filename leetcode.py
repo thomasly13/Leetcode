0996dc9f938bb2ed7def711bc46fc51244e4102b
Thomas Ly <Thomaslyy13@gmail.com>
2024-10-02 22:09:13 -0700

    added python make sum divisible by P

--- leetcode.py
+++ leetcode.py
@@ -865,3 +865,26 @@ class Solution:
         for key in self.myDict.keys():
             if self.myDict[key] == minVal:
                 return key
+    # Make Sum divisible by P
+        def minSubarray(self, nums: List[int], p: int) -> int:
+        totalSum = sum(nums)
+        rem = totalSum % p
+
+        if rem == 0:
+            return 0
+
+        prefixMod = {0: -1}
+        prefixSum = 0
+        minLength = len(nums)
+
+        for i, num in enumerate(nums):
+            prefixSum += num
+            currentMod = prefixSum % p
+            targetMod = (currentMod - rem + p) % p
+
+            if targetMod in prefixMod:
+                minLength = min(minLength, i - prefixMod[targetMod])
+
+            prefixMod[currentMod] = i
+
+        return minLength if minLength < len(nums) else -1
