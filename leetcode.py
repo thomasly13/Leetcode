7f703a40549baa6ed954be6882339b1a51037066
Thomas Ly <Thomaslyy13@gmail.com>
2024-10-14 21:41:13 -0700

    added ython seperate Black and White Balls

--- leetcode.py
+++ leetcode.py
@@ -1000,3 +1000,21 @@ class Solution:
                 group_count += 1
 
         return group_count
+
+    # Seperate Black and White Balls
+    
+    def minimum_steps(s):
+        swaps = 0
+        li = 0
+        ri = len(s) - 1
+        while li < ri:
+            if s[li] == '1':
+                if s[ri] == '0':
+                    swaps += ri - li
+                    li += 1
+                ri -= 1
+            else:
+                if s[ri] == '1':
+                    ri -= 1
+                li += 1
+        return swaps
