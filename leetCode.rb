4294b6431fbc209d8d9f3df4409904e0887be157
Thomas Ly <Thomaslyy13@gmail.com>
2024-10-24 21:18:23 -0700

    added ruby remove subfolders

--- leetCode.rb
+++ leetCode.rb
@@ -5702,3 +5702,25 @@ def kth_character(k)
     end
     dfs.call(root1, root2)
   end
+
+  # Remove Subfolders
+  def remove_subfolders(folder)
+    # Sort the folders lexicographically so parent folders come before their subfolders
+    folder.sort!
+    
+    # Initialize result array with the first folder
+    ans = [folder[0]]
+    
+    # Iterate through remaining folders starting from index 1
+    (1...folder.length).each do |i|
+        # Get the last added folder path and add a trailing slash
+        last_folder = ans[-1] + "/"
+        
+        # Check if current folder starts with last_folder
+        # If it doesn't start with last_folder, then it's not a subfolder
+        if !folder[i].start_with?(last_folder)
+            ans << folder[i]
+        end
+    end
+    
+    ans
