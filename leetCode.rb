2a580edea1182fd1e99743b1619e0e09cfe0423a
Thomas Ly <Thomaslyy13@gmail.com>
2024-10-31 12:30:06 -0700

    added ruby minimum total distance

--- leetCode.rb
+++ leetCode.rb
@@ -5761,3 +5761,34 @@ def kth_character(k)
         
         ans
     end
+
+    # Minimum total distance
+    def minimum_total_distance(robot, factory)
+        robot.sort!
+        factory.sort!
+        
+        m, n = robot.length, factory.length
+        dp = Array.new(m + 1) { Array.new(n + 1, 0) }
+        m.times { |i| dp[i][n] = Float::INFINITY }
+        
+        (n-1).downto(0) do |j|
+            prefix = 0
+            qq = [[m, 0]]
+            
+            (m-1).downto(0) do |i|
+                prefix += (robot[i] - factory[j][0]).abs
+                
+                qq.shift if qq[0][0] > i + factory[j][1]
+                
+                while !qq.empty? && qq[-1][1] >= dp[i][j+1] - prefix
+                    qq.pop
+                end
+                
+                qq.push([i, dp[i][j+1] - prefix])
+                dp[i][j] = qq[0][1] + prefix
+            end
+        end
+        
+        dp[0][0]
+    end
+    
