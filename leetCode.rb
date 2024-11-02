4e7cf1b6742f79d874b26dc8b579c19406aa46f9
Thomas Ly <Thomaslyy13@gmail.com>
2024-11-02 13:37:46 -0700

    added ruby circular sentence

--- leetCode.rb
+++ leetCode.rb
@@ -5792,3 +5792,23 @@ def kth_character(k)
         dp[0][0]
     end
     
+    # Circular sentence
+    def is_circular_sentence(sentence)
+        # Get the length of the sentence
+        n = sentence.length
+        
+        # First check: Compare first and last character of sentence
+        return false if sentence[0] != sentence[n-1]
+        
+        # Iterate through the sentence, starting from index 1 to n-2
+        (1..n-2).each do |i|
+          # When we find a space character
+          if sentence[i] == ' '
+            # Check if the character before space matches the character after space
+            return false if sentence[i-1] != sentence[i+1]
+          end
+        end
+        
+        # If we made it through all checks, the sentence is circular
+        true
+      end
