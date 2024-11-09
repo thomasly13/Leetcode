d1ec89f5ecd74c488bd884cb37a535589469fd1d
Thomas Ly <Thomaslyy13@gmail.com>
2024-11-08 21:23:30 -0800

    added ruby minimum array end

--- leetCode.rb
+++ leetCode.rb
@@ -5817,3 +5817,20 @@ def kth_character(k)
 def can_sort_array(nums)
     nums.chunk { _1.digits(2).sum }.flat_map { |_, chunk| chunk.sort } == nums.sort
 end
+
+# Minimum array end
+def min_end(n, x)
+    result = x
+    remaining = n - 1
+    position = 1
+    
+    while remaining != 0
+        if (x & position) == 0
+            result |= (remaining & 1) * position
+            remaining >>= 1
+        end
+        position <<= 1
+    end
+    
+    result.to_i
+end
