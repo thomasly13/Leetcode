367b03c844cfbee3d4030ffc50f6ce5240405b08
Thomas Ly <Thomaslyy13@gmail.com>
2024-11-16 20:09:37 -0800

    added python shortest subarray 3

--- leetcode.py
+++ leetcode.py
@@ -1252,3 +1252,26 @@ def minimumSubarrayLength(self, nums, k):
             left += 1
         
         return ans
+
+
+    # Shortest Sub Array 3
+        def shortestSubarray(self, nums, k):
+        n = len(nums)
+        prefixSum = [0] * (n + 1)
+        
+        for i in range(n):
+            prefixSum[i + 1] = prefixSum[i] + nums[i]
+        
+        dq = deque()
+        minLength = float('inf')
+        
+        for i in range(n + 1):
+            while dq and prefixSum[i] - prefixSum[dq[0]] >= k:
+                minLength = min(minLength, i - dq.popleft())
+            
+            while dq and prefixSum[i] <= prefixSum[dq[-1]]:
+                dq.pop()
+            
+            dq.append(i)
+        
+        return minLength if minLength != float('inf') else -1
